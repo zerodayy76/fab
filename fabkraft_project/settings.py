509dd22714f2b4a9520d2e226ec96c731a0590f6
boopathyganesh <boopathyganesh95@gmail.com>
2024-06-26 01:45:19 +0530

    DB Change SQLITE -> Supabase

--- fabkraft_project/settings.py
+++ fabkraft_project/settings.py
@@ -168,13 +168,23 @@ WSGI_APPLICATION = 'fabkraft_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.hsajciumqyoeapktcsqn',
+        'PASSWORD': 'nkcumJmuwJzsUstc',
+        'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
+        'PORT': '6543',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
